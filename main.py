97702f98855a410aacd1d3b88d3e8408258ee2a4
ghostface1111 <teomancopur12@gmail.com>
2024-11-19 16:41:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-meme_dict={
-    "КРИНЖ": "Что-то очень странное или стыдное",
-    "ЛОЛ": "Что-то очень смешное",
-    "РОФЛ": "шутка",  
-    "ЩИЩ": "одобрение или восторг",
-    "КРИПОВЫЙ": "страшный, пугающий",
-    "АГРИТЬСЯ": "злиться"
+import telebot
+from password import gen_pass
 
-} 
-while True:
-    word = input("Введите непонятное слово (большими буквами!): ").upper()
 
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("Добавим сейчас!")
-        value = input("введите значение "+word+": ")
-        meme_dict[word] = value
+token = ""
+
+ 
+bot = telebot.TeleBot(token)
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    # Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=["gen_pass"])
+def send_pass(message):
+   bot.reply_to(message, gen_pass(8))
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+print("bot start")
+bot.polling()
